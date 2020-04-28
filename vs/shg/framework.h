414f70a249aa707cd0646d1d9ecbd50bf0465803
Stanisław Galus <shgalus@gmail.com>
2020-04-28 05:56:28 +0200

    Dynamic library solution replaced by static library solution.

--- vs/shg/framework.h
+++
@@ -1,8 +0,0 @@
-#ifndef FRAMEWORK_H
-#define FRAMEWORK_H
-
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files
-#include <windows.h>
-
-#endif
