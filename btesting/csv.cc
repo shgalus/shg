6e9f7d2369921483da4cebb9f1e3769553f9469a
Stanisław Galus <shgalus@gmail.com>
2020-05-05 18:33:26 +0200

    Cleanup.

--- btesting/csv.cc
+++ btesting/csv.cc
@@ -1,147 +1,102 @@
+#include "shg/csv.h"
 #include <algorithm>
 #include <ios>
 #include <sstream>
-#include "shg/csv.h"
 #include <boost/test/unit_test.hpp>
 
-namespace SHG {
-namespace BTesting {
+namespace SHG::BTesting {
 
+using std::equal;
+using std::ios_base;
+using std::istringstream;
+using std::ostringstream;
 using std::string;
 using std::vector;
-using std::ostringstream;
-using std::istringstream;
-using std::ios_base;
-using std::equal;
 
 namespace {
 
-struct Test_data {
-     vector<vector<string>> raw;
-     string csv;
+struct Case {
+     const vector<vector<string>> raw;
+     const string csv;
 };
 
-const vector<Test_data> test_data {
-     {
-          {
-               {"10", "20", "30"},
-               {"40", "50", "60"},
-               {"70", "80", "90"}
-          },
-          {"10,20,30\r\n40,50,60\r\n70,80,90\r\n"}
-     },
-     {
-          {
-               {"10", ",abc", "30"},
-               {"40", "50", "60"},
-               {"70", "80", "90"}
-          },
-          {"10,\",abc\",30\r\n40,50,60\r\n70,80,90\r\n"}
-     },
-     {
-          {
-               {"10", ",a\r\nbc", "30"},
-               {"40", "50", "60"},
-               {"70", "80", "90"}
-          },
-          {"10,\",a\r\nbc\",30\r\n40,50,60\r\n70,80,90\r\n"}
-     },
-     {
-          {
-               {"1\"0", "2\"0", "30"},
-               {"40", "50", "60"},
-               {"70", "80", "90"}
-          },
-          {"\"1\"\"0\",\"2\"\"0\",30\r\n40,50,60\r\n70,80,90\r\n"}
-     },
-     {
-          {
-               {"1\"0", "2\"\"0", "30"},
-               {"40", "50", "60"},
-               {"70", "80", "90"}
-          },
-          {"\"1\"\"0\",\"2\"\"\"\"0\",30\r\n40,50,60\r\n70,80,90\r\n"}
-     },
-     {
-          {
-               {"10", "20", "30"},
-               {"40", "a,b\"c\rd\ne\"\"", "60"},
-               {"70", "80", "90"}
-          },
-          {"10,20,30\r\n40,\"a,b\"\"c\rd\ne\"\"\"\"\",60\r\n70,80,90"
-           "\r\n"}
-     },
-     {
-          {
-               {"10", "20", "30"},
-               {"40", "", ""},
-               {"70", "80", "90"}
-          },
-          {"10,20,30\r\n40,,\r\n70,80,90\r\n"}
-     },
-     {
-          {
-               {"{\r\n"
-                "  \"title\": \"Films\",\r\n"
-                "  [\r\n"
-                "    {\r\n"
-                "      \"director\": \"Jerzy Antczak\",\r\n"
-                "      \"title\": \"Noce i dnie\",\r\n"
-                "      \"year\": 1975\r\n"
-                "    },\r\n"
-                "    {\r\n"
-                "      \"director\": \"Andrzej Wajda\",\r\n"
-                "      \"title\": \"Korczak\",\r\n"
-                "      \"year\": 1990\r\n"
-                "    },\r\n"
-                "    {\r\n"
-                "      \"director\": \"Wojciech Marczewski\",\r\n"
-                "      \"title\": \"Zmory\",\r\n"
-                "      \"year\": 1978\r\n"
-                "    }\r\n"
-                "  ]\r\n"
-                "}\r\n"}
-          },
-          {"\"{\r\n"
-           "  \"\"title\"\": \"\"Films\"\",\r\n"
-           "  [\r\n"
-           "    {\r\n"
-           "      \"\"director\"\": \"\"Jerzy Antczak\"\",\r\n"
-           "      \"\"title\"\": \"\"Noce i dnie\"\",\r\n"
-           "      \"\"year\"\": 1975\r\n"
-           "    },\r\n"
-           "    {\r\n"
-           "      \"\"director\"\": \"\"Andrzej Wajda\"\",\r\n"
-           "      \"\"title\"\": \"\"Korczak\"\",\r\n"
-           "      \"\"year\"\": 1990\r\n"
-           "    },\r\n"
-           "    {\r\n"
-           "      \"\"director\"\": \"\"Wojciech Marczewski\"\",\r\n"
-           "      \"\"title\"\": \"\"Zmory\"\",\r\n"
-           "      \"\"year\"\": 1978\r\n"
-           "    }\r\n"
-           "  ]\r\n"
-           "}\r\n"
-           "\"\r\n"}
-     },
-     {
-          {
-               {"10", "20", "30"},
-               {"", "", ""},
-               {"70", "80", "90"}
-          },
-          {"10,20,30\r\n,,\r\n70,80,90\r\n"}
-     },
+const vector<Case> vc{
+     {{{"10", "20", "30"}, {"40", "50", "60"}, {"70", "80", "90"}},
+      {"10,20,30\r\n40,50,60\r\n70,80,90\r\n"}},
+     {{{"10", ",abc", "30"}, {"40", "50", "60"}, {"70", "80", "90"}},
+      {"10,\",abc\",30\r\n40,50,60\r\n70,80,90\r\n"}},
+     {{{"10", ",a\r\nbc", "30"},
+       {"40", "50", "60"},
+       {"70", "80", "90"}},
+      {"10,\",a\r\nbc\",30\r\n40,50,60\r\n70,80,90\r\n"}},
+     {{{"1\"0", "2\"0", "30"},
+       {"40", "50", "60"},
+       {"70", "80", "90"}},
+      {"\"1\"\"0\",\"2\"\"0\",30\r\n40,50,60\r\n70,80,90\r\n"}},
+     {{{"1\"0", "2\"\"0", "30"},
+       {"40", "50", "60"},
+       {"70", "80", "90"}},
+      {"\"1\"\"0\",\"2\"\"\"\"0\",30\r\n40,50,60\r\n70,80,90\r\n"}},
+     {{{"10", "20", "30"},
+       {"40", "a,b\"c\rd\ne\"\"", "60"},
+       {"70", "80", "90"}},
+      {"10,20,30\r\n40,\"a,b\"\"c\rd\ne\"\"\"\"\",60\r\n70,80,90"
+       "\r\n"}},
+     {{{"10", "20", "30"}, {"40", "", ""}, {"70", "80", "90"}},
+      {"10,20,30\r\n40,,\r\n70,80,90\r\n"}},
+     {{{"{\r\n"
+        "  \"title\": \"Films\",\r\n"
+        "  [\r\n"
+        "    {\r\n"
+        "      \"director\": \"Jerzy Antczak\",\r\n"
+        "      \"title\": \"Noce i dnie\",\r\n"
+        "      \"year\": 1975\r\n"
+        "    },\r\n"
+        "    {\r\n"
+        "      \"director\": \"Andrzej Wajda\",\r\n"
+        "      \"title\": \"Korczak\",\r\n"
+        "      \"year\": 1990\r\n"
+        "    },\r\n"
+        "    {\r\n"
+        "      \"director\": \"Wojciech Marczewski\",\r\n"
+        "      \"title\": \"Zmory\",\r\n"
+        "      \"year\": 1978\r\n"
+        "    }\r\n"
+        "  ]\r\n"
+        "}\r\n"}},
+      {"\"{\r\n"
+       "  \"\"title\"\": \"\"Films\"\",\r\n"
+       "  [\r\n"
+       "    {\r\n"
+       "      \"\"director\"\": \"\"Jerzy Antczak\"\",\r\n"
+       "      \"\"title\"\": \"\"Noce i dnie\"\",\r\n"
+       "      \"\"year\"\": 1975\r\n"
+       "    },\r\n"
+       "    {\r\n"
+       "      \"\"director\"\": \"\"Andrzej Wajda\"\",\r\n"
+       "      \"\"title\"\": \"\"Korczak\"\",\r\n"
+       "      \"\"year\"\": 1990\r\n"
+       "    },\r\n"
+       "    {\r\n"
+       "      \"\"director\"\": \"\"Wojciech Marczewski\"\",\r\n"
+       "      \"\"title\"\": \"\"Zmory\"\",\r\n"
+       "      \"\"year\"\": 1978\r\n"
+       "    }\r\n"
+       "  ]\r\n"
+       "}\r\n"
+       "\"\r\n"}},
+     {{{"10", "20", "30"}, {"", "", ""}, {"70", "80", "90"}},
+      {"10,20,30\r\n,,\r\n70,80,90\r\n"}},
 };
 
-}       // anonymous namespace
+}  // anonymous namespace
 
-BOOST_AUTO_TEST_SUITE(csv)
+BOOST_AUTO_TEST_SUITE(csv_test)
 
-BOOST_AUTO_TEST_CASE(csv_writer) {
+BOOST_AUTO_TEST_CASE(csv_writer_test) {
      ostringstream ss(ios_base::out | ios_base::binary);
      CSV_writer csvw(ss);
-     for (const auto& td : test_data) {
+     for (const auto& td : vc) {
           ss.str("");
           for (const auto& r : td.raw)
                csvw.write_record(r);
@@ -149,11 +104,9 @@ BOOST_AUTO_TEST_CASE(csv_writer) {
      }
 }
 
-BOOST_AUTO_TEST_CASE(csv_reader) {
-     for (const auto& td : test_data) {
-          istringstream ss(
-               td.csv,
-               ios_base::in | ios_base::binary);
+BOOST_AUTO_TEST_CASE(csv_reader_test) {
+     for (const auto& c : vc) {
+          istringstream ss(c.csv, ios_base::in | ios_base::binary);
           CSV_reader csvr(ss);
           vector<vector<string>>::size_type k = 0;
           vector<string> v;
@@ -161,21 +114,21 @@ BOOST_AUTO_TEST_CASE(csv_reader) {
                csvr.getrec(v);
                if (v.size() == 0)
                     break;
-               while (k < td.raw.size() && td.raw[k].size() == 0)
+               while (k < c.raw.size() && c.raw[k].size() == 0)
                     k++;
-               if (k == td.raw.size())
+               if (k == c.raw.size())
                     break;
-               BOOST_CHECK(k < td.raw.size());
-               BOOST_CHECK(v.size() == td.raw[k].size());
-               BOOST_CHECK(equal(v.cbegin(), v.cend(),
-                                 td.raw[k].cbegin()));
+               BOOST_CHECK(k < c.raw.size());
+               BOOST_CHECK(v.size() == c.raw[k].size());
+               BOOST_CHECK(
+                    equal(v.cbegin(), v.cend(), c.raw[k].cbegin()));
                k++;
           }
-          BOOST_CHECK(k == td.raw.size());
+          BOOST_CHECK(k == c.raw.size());
      }
 }
 
-BOOST_AUTO_TEST_CASE(empty_input) {
+BOOST_AUTO_TEST_CASE(empty_input_test) {
      istringstream ss(ios_base::in | ios_base::binary);
      CSV_reader csvr(ss);
      vector<string> v;
@@ -184,7 +137,7 @@ BOOST_AUTO_TEST_CASE(empty_input) {
      BOOST_CHECK(v.size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE(without_end_of_record) {
+BOOST_AUTO_TEST_CASE(without_end_of_record_test) {
      istringstream ss(ios_base::in | ios_base::binary);
      CSV_reader csvr(ss);
      vector<string> v;
@@ -196,7 +149,7 @@ BOOST_AUTO_TEST_CASE(without_end_of_record) {
      BOOST_CHECK(v.size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE(only_end_of_record) {
+BOOST_AUTO_TEST_CASE(only_end_of_record_test) {
      istringstream ss(ios_base::in | ios_base::binary);
      CSV_reader csvr(ss);
      vector<string> v;
@@ -208,7 +161,7 @@ BOOST_AUTO_TEST_CASE(only_end_of_record) {
      BOOST_CHECK(v.size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE(field_separator_followed_by_end_of_record) {
+BOOST_AUTO_TEST_CASE(field_separator_followed_by_end_of_record_test) {
      istringstream ss(ios_base::in | ios_base::binary);
      CSV_reader csvr(ss);
      vector<string> v;
@@ -223,5 +176,4 @@ BOOST_AUTO_TEST_CASE(field_separator_followed_by_end_of_record) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
-}       // namespace BTesting
-}       // namespace SHG
+}  // namespace SHG::BTesting
