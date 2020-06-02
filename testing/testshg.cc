51b25790701c0b18f2fbd8488f31c50484998687
Stanisław Galus <shgalus@gmail.com>
2020-06-02 20:00:33 +0200

    Testing normmix moved to btesting.

--- testing/testshg.cc
+++ testing/testshg.cc
@@ -177,7 +177,6 @@ Test_shg::Test_shg() : mm() {
      register_test(test_except, "except");
      register_test(test_gsgts, "gsgts");
      register_test(test_hmm, "hmm");
-     register_test(test_normmix, "normmix");
      register_test(test_opdts, "opdts");
      register_test(test_permentr, "permentr");
      register_test(test_smc, "smc");
