1ce6040534b69d04d88c8978a152e4422c5ac860
Stanisław Galus <shgalus@gmail.com>
2020-06-02 21:20:34 +0200

    Empty file smc_test.cc added.

---
+++ btesting/smc_test.cc
@@ -0,0 +1,15 @@
+#include "shg/smc.h"
+#include <boost/test/unit_test.hpp>
+
+namespace SHG::BTesting {
+
+BOOST_AUTO_TEST_SUITE(smc_test)
+
+BOOST_AUTO_TEST_CASE(basic_test) {
+     BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace SHG::BTesting
+
