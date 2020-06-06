03a4a5c3808057f1f3eb91ede096fea3cdcb68a5
Stanisław Galus <shgalus@gmail.com>
2020-06-06 20:13:47 +0200

    Gsgts_test.cc started.

---
+++ btesting/gsgts_test.cc
@@ -0,0 +1,17 @@
+#include "shg/gsgts.h"
+#include <cmath>
+#include <boost/test/unit_test.hpp>
+#include "shg/mzt.h"
+#include "shg/utils.h"
+
+namespace SHG::BTesting {
+
+BOOST_AUTO_TEST_SUITE(gsgts_test)
+
+BOOST_AUTO_TEST_CASE(basic_test) {
+     BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace SHG::BTesting
